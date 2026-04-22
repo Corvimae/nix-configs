5f775b2a66a5df9f353a816811cd2c14a5eb85da
May Roussel <may@maybreak.com>
2026-04-21 23:29:11 -0500

    eh

--- modules/darwin/programs/tailscale.nix
+++ modules/darwin/programs/tailscale.nix
@@ -4,14 +4,19 @@ let
   cfg = config.may.programs.tailscale;
 in {
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      (tailscale-gui.overrideAttrs (oldAttrs: rec {
-        version = "1.96.5";
-        src = pkgs.fetchurl {
-          url = "https://pkgs.tailscale.com/stable/Tailscale-${version}-macos.pkg";
-          hash = "sha256-eqwNX5uBGOiy1z91eMMkE6892ot+eXlM5/6jHm9uF8g=";
-        };
-      }))
-    ];
+    # todo: re-enable this to get the tray icon back once the versions are properly synced
+    # https://github.com/NixOS/nixpkgs/pull/484852 <-- dead pr :(
+        
+    # environment.systemPackages = with pkgs; [
+    #   (tailscale-gui.overrideAttrs (oldAttrs: rec {
+    #     version = "1.96.5";
+    #     src = pkgs.fetchurl {
+    #       url = "https://pkgs.tailscale.com/stable/Tailscale-${version}-macos.pkg";
+    #       hash = "sha256-eqwNX5uBGOiy1z91eMMkE6892ot+eXlM5/6jHm9uF8g=";
+    #     };
+    #   }))
+    # ];
+
+    services.tailscale.enable = true;
   };
 }
