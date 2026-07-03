cd54268561966c5356f293038ba64b646e6210cb
May Roussel <may@maybreak.com>
2026-07-03 13:30:07 -0500

    teleport

--- modules/home/programs/zsh.nix
+++ modules/home/programs/zsh.nix
@@ -38,6 +38,11 @@ in {
         )
 
         BULLETTRAIN_CONTEXT_DEFAULT_USER=${config.home.username}
+
+        export TELEPORT_LOGIN='Corvimae'
+        export TELEPORT_PROXY='teleport.gamesdonequick.com:443'
+        export TELEPORT_AUTH_SERVER='teleport.gamesdonequick.com:443'
+        export TELEPORT_AUTH='github'
       '';
     };
   };
