e09cf18c6bf5a3b9fdb740fbdb5fac3be26c14b2
May Roussel <may@maybreak.com>
2026-07-20 13:57:14 -0500

    move gdq shortcuts to archen-only

--- hosts/Archen/default.nix
+++ hosts/Archen/default.nix
@@ -4,7 +4,23 @@
   imports = [];
 
   # dunno why it wants this so badly
-  users.users.may-darwin.home = "/Users/may";
+  users.users.may-darwin = {
+    home = "/Users/may";
+  };
 
   may = pkgs.mayUtils.loadConfig "Archen" ../../config.toml;
+
+  home-manager.users.may-darwin = {
+    programs.zsh.shellAliases = {
+      # doctl shortcuts
+      ff-context = "kubectl config use-context do-nyc1-ff-k8s";
+      maybreak-context = "kubectl config use-context do-nyc1-maybreak-k8s";
+      board-game-manager-context = "kubectl config use-context do-nyc1-board-game-manager";
+      gdq-context = "kubectl config use-context do-nyc1-k8s-gdq-api";
+      tracker-context = "kubectl config use-context do-nyc1-gdq-donation-tracker";
+
+      # ssh shortcuts
+      studio-nodecg = "ssh -p 2010 gdq-studio-server";
+    };
+  };
 }
--- modules/darwin/homes/may-darwin/default.nix
+++ modules/darwin/homes/may-darwin/default.nix
@@ -15,16 +15,6 @@
     programs.zsh.shellAliases = {
       renix = "sudo darwin-rebuild switch --flake ~/Projects/nix-configs";
       nix-repl = "nix repl --extra-experimental-features 'flakes' nixpkgs";
-
-      # doctl shortcuts
-      ff-context = "kubectl config use-context do-nyc1-ff-k8s";
-      maybreak-context = "kubectl config use-context do-nyc1-maybreak-k8s";
-      board-game-manager-context = "kubectl config use-context do-nyc1-board-game-manager";
-      gdq-context = "kubectl config use-context do-nyc1-k8s-gdq-api";
-      tracker-context = "kubectl config use-context do-nyc1-gdq-donation-tracker";
-
-      # ssh shortcuts
-      studio-nodecg = "ssh -p 2010 gdq-studio-server";
     };
   };
 }
