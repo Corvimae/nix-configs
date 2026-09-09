b282ba509a416d9d0cf250a678bfe8e4ca8e5458
Corvimae <may@maybreak.com>
2026-09-09 13:44:27 -0500

    ^ for standalone as well

--- modules/standalone/default.nix
+++ modules/standalone/default.nix
@@ -37,6 +37,9 @@ in {
               "may"
               "@wheel"
             ];
+
+            extra-substituters = [ "https://noctalia.cachix.org" ];
+            extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
           };
         };
 
